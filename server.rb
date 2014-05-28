37e1cf51845b2cd3af4099133d101346c8942d80
Rachael Serur <rachaelse@gmail.com>
2014-05-28 15:17:59 -0400

    Successfully sorts by rating or year, with working pagination in either sort type.

--- server.rb
+++ server.rb
@@ -43,8 +43,6 @@ get '/actors/:id' do
 
   @query = params[:id]
 
-
-
    db_connection do |conn|
     @roles = conn.exec('SELECT actors.name, movies.title, cast_members.character
       FROM actors JOIN cast_members ON actors.id = cast_members.actor_id JOIN movies ON
@@ -68,36 +66,41 @@ get '/movies' do
   if @order == "year"
 
      db_connection do |conn|
-    @movies = conn.exec('SELECT movies.id, movies.title, movies.year, movies.rating,
+
+      @movies = conn.exec('SELECT movies.id, movies.title, movies.year, movies.rating,
       genres.name, studios.name FROM movies JOIN genres ON genres.id = movies.genre_id
-      JOIN studios ON studios.id = movies.studio_id ORDER BY movies.year ASC')
-    @movies = @movies.values
+      JOIN studios ON studios.id = movies.studio_id ORDER BY movies.year ASC LIMIT 20 OFFSET $1', [@page * 20])
+
+      @movies = @movies.values
+
     end
 
   elsif @order == "rating"
 
    db_connection do |conn|
+
       @movies = conn.exec('SELECT movies.id, movies.title, movies.year, movies.rating,
         genres.name, studios.name FROM movies JOIN genres ON genres.id = movies.genre_id
         JOIN studios ON studios.id = movies.studio_id WHERE movies.rating IS NOT NULL
-        ORDER BY movies.rating DESC')
+        ORDER BY movies.rating DESC LIMIT 20 OFFSET $1', [@page * 20])
+
       @movies = @movies.values
     end
+
   else
 
      db_connection do |conn|
+
     @movies = conn.exec('SELECT movies.id, movies.title, movies.year, movies.rating,
       genres.name, studios.name FROM movies JOIN genres ON genres.id = movies.genre_id
-      JOIN studios ON studios.id = movies.studio_id')
+      JOIN studios ON studios.id = movies.studio_id LIMIT 20 OFFSET $1', [@page * 20])
     @movies = @movies.values
+
     end
 
   end
 
   @movies = @movies.to_a
-
-  index = @page.to_i * 20
-  @movies = @movies.slice(index,20)
 
   erb :movies
 
